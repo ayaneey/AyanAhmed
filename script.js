0354d5b8c026501034dceb9fc99e702628766714
ayaneey <ayan.h18@hotmail.com>
2022-10-08 23:57:47 +0100

    aligned margins and paddings

---
+++ script.js
@@ -0,0 +1,6 @@
+// function reset_animation() {
+//   var el = document.getElementsByClassName("back");
+//   el.style.animation = "none";
+//   el.offsetHeight; /* trigger reflow */
+//   el.style.animation = null;
+// }
